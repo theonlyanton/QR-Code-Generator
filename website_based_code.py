99fd0f14f2a121b2317217e34f393ee35336713c
Anton Krotenok <80707699+theonlyanton@users.noreply.github.com>
2021-08-17 17:03:21 -0400

    Update website_based_code.py

--- website_based_code.py
+++ website_based_code.py
@@ -5,6 +5,5 @@ import qrcode
 # enter your URL link within the .make method
 img = qrcode.make("https://www.youtube.com/")
 
-# create and save your QR code inside a variable
 # name the QR code within the .save method
 final_image = img.save("youtubeQR.jpg")
